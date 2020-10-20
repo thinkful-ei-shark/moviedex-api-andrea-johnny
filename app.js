92e259326dceab829052f93019bc41eee617e8cd
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-10-20 14:03:49 -0500

    added genre filter

--- app.js
+++ app.js
@@ -5,4 +5,21 @@ const app = express();
 
 app.use(morgan('common'));
 
+const movies = require('./movies-data-small');
+
+app.get('/movie', (req, res, next) => {
+  const {genre, country, avg_vote} = req.query;
+
+  let movieList = [...movies];
+
+  if(genre) {
+    movieList = movieList.filter(movie => {
+      return movie.genre.toLowerCase() === genre.toLowerCase();
+    });
+  }
+
+  res.json(movieList);
+
+});
+
 app.listen(8080, () => console.log('server started'));
