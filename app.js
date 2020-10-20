906a5d414122dc60de7dd14f24ec47fe22c88671
Andrea Pullen <apullenb@gmail.com>
2020-10-20 15:08:13 -0500

    final

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
-
+require('dotenv').config();
+const cors = require('cors');
 const app = express();
-
+const helmet= require('helmet');
 app.use(morgan('common'));
+app.use(cors());
+app.use(helmet());
 
+app.use(function validateBearerToken(req, res, next) {
+  let token=process.env.API_TOKEN;
+  let entToken=req.get('Authorization');
+  if(!entToken || entToken.split(' ')[1] !== token) {
+    return res.status(400).json({error:'invalid api token'});
+  }
+  next();
+});
 const movies = require('./movies-data-small');
 
 app.get('/movie', (req, res, next) => {
@@ -33,5 +44,4 @@ app.get('/movie', (req, res, next) => {
   res.json(movieList);
 
 });
-
 app.listen(8080, () => console.log('server started'));
